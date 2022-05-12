d1cf93796a3e3812135f815ee3b898c77b7515cd
Thur4321 <arthurhoa@gmail.com>
2022-05-12 17:44:23 -0300

    AppMensagem

---
+++ App Mensagem/src/app/mensagem/Mensagem.java
@@ -0,0 +1,18 @@
+
+package app.mensagem;
+
+public class Mensagem {
+
+//Atributo//    
+    private String textoDaMensagem="BOA NOITE!\nSEJAM TODOS BEM-VINDOS";
+    
+//Método//    
+    public void exibeMensagem(){
+    System.out.println(textoDaMensagem);
+    
+    }
+    
+}
+
+
+
---
+++ App Mensagem/src/app/mensagem/Principal.java
@@ -0,0 +1,13 @@
+
+package app.mensagem;
+//Java inicia execução pelo main//
+
+public class Principal {
+   
+   public static void main(String[] args) {
+   Mensagem msg=new Mensagem();
+   msg.exibeMensagem();
+    }
+   
+}
+    
